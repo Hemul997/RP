c77a9d1fca505c4d287a17c8f06e01ab44536007
Hemul997 <nikitamoshkin1997@gmail.com>
2018-03-16 14:10:38 +0300

    Файлы программы и скрипты перенесены в отдельную папку src

---
+++ Lab2/src/Build.cmd
@@ -0,0 +1,17 @@
+@echo off
+if %~1 == "" goto err
+
+start /wait /d Frontend dotnet publish -o ..\..\Builds\%~1\Frontend
+start /wait /d Backend dotnet publish -o ..\..\Builds\%~1\Backend
+
+start /wait xcopy config ..\Builds\%~1\config /I
+start /wait xcopy run.cmd ..\Builds\%~1
+start /wait xcopy stop.cmd ..\Builds\%~1
+
+echo "Compilation and build were completed"
+pause
+exit 0
+
+:err
+echo "Arguments was not found"
+exit 1
