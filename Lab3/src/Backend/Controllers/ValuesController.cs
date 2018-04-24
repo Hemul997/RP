9b2701af3f36e766f957141bf19ecbd094aa6c14
Hemul997 <nikitamoshkin1997@gmail.com>
2018-04-24 21:51:58 +0300

    Добавил третью лабораторную работу

---
+++ Lab3/src/Backend/Controllers/ValuesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using StackExchange.Redis;
+
+namespace Backend.Controllers
+{
+    
+    public class DataObject {
+        public string data {get; set;}
+    }
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        const string hostName = "localhost";
+        const string exchangeName = "backend-api"; 
+        //static readonly ConcurrentDictionary<string, string> _data = new ConcurrentDictionary<string, string>();
+        private static IConnectionMultiplexer redisChannel = ConnectionMultiplexer.Connect(hostName);
+        IDatabase database = redisChannel.GetDatabase();
+
+
+        // GET api/values/<id>
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            string value = null;
+            //_data.TryGetValue(id, out value);
+            database.StringGet(id);
+            return value;
+        }
+
+        private static void AddIdToExchange(string id) 
+        {
+            IConnectionFactory factory = new ConnectionFactory
+            {
+                HostName = hostName
+            };
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare(exchangeName, false, false, false, null);
+
+            string message = id;
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                        routingKey: exchangeName,
+                        basicProperties: null,
+                        body: body);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post(string value)
+        {
+            if (value != null) {
+                Console.WriteLine(value);
+            }
+            var id = Guid.NewGuid().ToString();
+            database.StringSet(id, value);
+            AddIdToExchange(id);
+            //_data[id] = value;
+            return id;
+        }
+    }
+}
---
+++ Lab3/src/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Headers;
+
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload(string data)
+        {
+            string id = "";
+            //TODO: send data in POST request to backend and read returned id value from response
+            string url = "http://127.0.0.1:5000/api/values";
+            if(data != null)
+            {
+                id = Post(url, data).Result;
+            }
+            
+            return Ok(id);
+        }
+
+        private async Task<string> Post(string url, string data)
+        {
+            var httpClient = new HttpClient();
+            var content = new FormUrlEncodedContent(new[] { 
+                new KeyValuePair<string, string>("", data) 
+            }); 
+            var response = await httpClient.PostAsync(url, content);
+            var id = await response.Content.ReadAsStringAsync();
+
+            return id;
+        }  
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
